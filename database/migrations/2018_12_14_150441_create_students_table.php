a28ac6050081bc624903731d4521d1af7197aa87
javas <chainoflogic@gmail.com>
2018-12-14 20:39:03 +0500

    all migrations

---
+++ database/migrations/2018_12_14_150441_create_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('second_name');
+            $table->date('birth_date');
+            $table->string('phone_number');
+            $table->string('address');
+            $table->string('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
