3b7a685837c516d9b85bac19906931c3dc3f3575
javas <chainoflogic@gmail.com>
2018-12-14 19:59:18 +0500

    auth and routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,41 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Auth::routes();
+
+
+// Students main page
+Route::get('/students', 'StudentController@showStudents');
+// Delete student from db
+Route::post('/delstudent/{stud_id}', 'StudentController@deleteStudent')->where('stud_id', '[0-9]+');
+// Modify student
+Route::post('/modstudent/{stud_id}', 'StudentController@modifyStudent')->where('stud_id', '[0-9]+');
+// Add student
+Route::post('/addstudent', 'StudentController@addStudent');
+// Show student's courses
+Route::get('/students/{stud_id}', 'StudentController@showStudentCourses')->where('stud_id', '[0-9]+');
+
+
+// Courses main page
+Route::get('/courses', 'CourseController@showCourses');
+// Delete course from db
+Route::post('/delcourse/{course_id}', 'CourseController@deleteCourse')->where('course_id', '[0-9]+');
+// Modify course
+Route::post('/modcourse/{course_id}', 'CourseController@modifyCourse')->where('course_id', '[0-9]+');
+// Add course
+Route::post('/addcourse', 'CourseController@addCourse');
+// Show course's students
+Route::get('/courses/{course_id}', 'CourseController@showCourseStudents')->where('course_id', '[0-9]+');
+
+
+
+// Delete a course from a user
+Route::post('/students/{scourse_id}', 'StudentsController@deleteStudentCourse')->where('scourse_id', '[0-9]+');
+// Bind a student to a course
+Route::post('/students/{course_id}/{stud_id}', 'StudentsController@addStudentCourse')->where(['course_id' => '[0-9]+', 'stud_id' => '[0-9]+']);
+
+
+
+
+Route::get('/home', 'HomeController@index')->name('home');
